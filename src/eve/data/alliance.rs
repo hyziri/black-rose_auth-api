d3196b2dc2ea88aa8104db70aba932b0890ca2d1
hyziri <hyziri@protonmail.com>
2024-05-16 14:02:56 -0500

    Alliance repository

--- src/eve/data/alliance.rs
+++ src/eve/data/alliance.rs
@@ -7,6 +7,48 @@ use sea_orm::{
 use entity::eve_alliance::Model as Alliance;
 use entity::prelude::EveAlliance;
 
+pub struct AllianceRepository {
+    db: DatabaseConnection,
+}
+
+impl AllianceRepository {
+    pub fn new(db: DatabaseConnection) -> Self {
+        Self { db }
+    }
+
+    pub async fn create(
+        &self,
+        alliance_id: i32,
+        alliance_name: String,
+        executor_corporation: Option<i32>,
+    ) -> Result<Alliance, sea_orm::DbErr> {
+        let new_alliance = entity::eve_alliance::ActiveModel {
+            alliance_id: ActiveValue::Set(alliance_id),
+            alliance_name: ActiveValue::Set(alliance_name),
+            executor: ActiveValue::Set(executor_corporation),
+            ..Default::default()
+        };
+
+        new_alliance.insert(&self.db).await
+    }
+
+    pub async fn get_one(&self, alliance_id: i32) -> Result<Option<Alliance>, sea_orm::DbErr> {
+        EveAlliance::find()
+            .filter(entity::eve_alliance::Column::AllianceId.eq(alliance_id))
+            .one(&self.db)
+            .await
+    }
+
+    pub async fn get_many(&self, alliance_ids: &[i32]) -> Result<Vec<Alliance>, sea_orm::DbErr> {
+        let alliance_ids: Vec<sea_orm::Value> = alliance_ids.iter().map(|&id| id.into()).collect();
+
+        EveAlliance::find()
+            .filter(entity::eve_alliance::Column::AllianceId.is_in(alliance_ids))
+            .all(&self.db)
+            .await
+    }
+}
+
 pub async fn get_alliance(
     db: &DatabaseConnection,
     alliance_id: i32,
